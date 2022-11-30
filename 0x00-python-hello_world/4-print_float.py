6ad99a84256afc1f5d797e31495dae155805f82b
SeasonLeague5 <facebookups23@gmail.com>
2022-11-30 10:31:26 +0000

    ALX

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-<<<<<<< HEAD
-print('Float: {number:.2f}')
-=======
-print(f'Float: {number:.2f}')
->>>>>>> 5fe258898e9c89f43c666286084cb9d21a153eef
+print("Float: {:0.2f}".format(number))
