c85eb8d2b27c5e00a8fb34b1f6104037ba96c6c5
SeasonLeague5 <facebookups23@gmail.com>
2022-11-30 09:50:22 +0000

    Laughing

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 or i % 5 == 0:
-            if i % 3 == 0:
-                print("{}".format("Fizz"), end="")
-            if i % 5 == 0:
-                print("{}".format("Buzz"), end="")
-            else:
-                print("{}".format(i), end="")
-                print(" ", end="")
+        if i % 3 == 0 and i % 5 == 0:
+	    print("FizzBuzz", end=" ")
+	elif i % 5 == 0:
+	    print("Buzz", end=" ")
+	elif i % 3 == 0:
+	    print("Fizz", end=" ")
+	else:
+	    print("{:d}".format(i), end=" ")
 fizzbuzz()
