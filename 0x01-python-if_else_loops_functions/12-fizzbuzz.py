493bbf47f55f99919dc6f68ac7eb321358c9159a
SeasonLeague5 <facebookups23@gmail.com>
2022-11-30 09:47:45 +0000

    :potted_plant:

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
+
 def fizzbuzz():
-        for i in range(1, 101):
-                if i % 3 == 0 or i % 5 == 0:
-                        if i % 3 == 0:
-                                print("{}".format("Fizz"), end="")
-                        if i % 5 == 0:
-                                print("{}".format("Buzz"), end="")
-                else:
-                        print("{}".format(i), end="")
+    for i in range(1, 101):
+        if i % 3 == 0 or i % 5 == 0:
+            if i % 3 == 0:
+                print("{}".format("Fizz"), end="")
+            if i % 5 == 0:
+                print("{}".format("Buzz"), end="")
+            else:
+                print("{}".format(i), end="")
                 print(" ", end="")
 fizzbuzz()
