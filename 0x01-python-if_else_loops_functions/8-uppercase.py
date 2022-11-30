ceb27b7bd709ad8ab9230936d69266441d710b6b
SeasonLeague5 <facebookups23@gmail.com>
2022-11-30 09:33:11 +0000

    Trying to get resources

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
-
-
+ #!/usr/bin/python3
 def uppercase(str):
-    result = ''
-    for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            result += chr(ord(char) - 32)
-        else:
-            result += char
-        print("{:s}".format(result))
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+    print("{:s}".format(i), end="")
+    print()
