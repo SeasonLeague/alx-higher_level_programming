6c09b18e54995fc42fb30dc7195247eadaefdb04
SeaonLeague <facebookups23@gmail.com>
2022-12-05 06:30:29 -0800

    syntaxs

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx, element):
-    tmp_list = my_list[:]
-    if 0 <= idx < len(my_list):
-        tmp_list[idx] = element 
-        return(tmp_list)
-    return(new_list)
+    new_list = my_list[:]
+    if idx >= 0 and idx < len(new_list):
+        new_list[idx] = element
+    return new_list
