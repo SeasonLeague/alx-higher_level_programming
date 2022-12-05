12a5c5476e2ac9333fc5437d8d9b30d076d6f076
SeaonLeague <facebookups23@gmail.com>
2022-12-05 06:20:16 -0800

    Fixed syntax error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-
 def replace_in_list(my_list, idx, element):
-    """Replaces element of a list at specified index"""
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
-        return my_list 
+    return my_list 
