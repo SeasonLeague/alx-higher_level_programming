81bd8e9b1156a306fcc87b8aee9d5ba11ad18d6e
SeaonLeague <facebookups23@gmail.com>
2022-12-06 03:53:02 -0800

    Another fix[]101

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of ints"""
     for row in matrix:
-        for i in row:
-            if i != row[-1]:
-                print("{:d}".format(i), end="")
-            else:
-                print("{:d}".format(i), end="")
-        print()
-
-print_matrix_integer(matrix=[[]])
+        print(" ".join("{:d}".format(el) for el in row))
