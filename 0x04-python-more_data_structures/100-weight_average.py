99a7df8195ca2c7a49e9f40803780fb9761b12a8
SeaonLeague <facebookups23@gmail.com>
2022-12-07 10:13:59 -0800

    ALX-ALX

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3 
 def weight_average(my_list=[]):
-    return sum([x[0] * x[1] for x in my_list]) / \
-        (sum([x[1] for x in my_list]) or 1)
+    if not  my_list:
+        return 0
+    else:
+        return(sum(a * b for a, b in my_list) / sum(b for a, b in my_list))
