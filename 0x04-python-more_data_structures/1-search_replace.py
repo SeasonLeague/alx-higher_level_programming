6c908db1284709df21203d515b8da91c2b429851
SeaonLeague <facebookups23@gmail.com>
2022-12-07 08:32:10 -0800

    Hello1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,4 @@
 
 
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-        for i in range(len(new_list)):
-            if new_list[i] == search:
-                new_list[i] = replace
-        return (new_list)
+    return list(map(lambda e: replace if e == search else e, my_list))
