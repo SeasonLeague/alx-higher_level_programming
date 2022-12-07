db682e99131b3b1dc278c4fa41be4da1e7c1b9c9
SeaonLeague <facebookups23@gmail.com>
2022-12-07 08:29:03 -0800

    Lets go2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    tmp = []
-    for x in matrix:
-        tmp.append(list(map(lambda x: x**2, x)))
-    return(tmp)
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
