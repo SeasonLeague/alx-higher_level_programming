6b2d60bb5edcd6355d2655fc295559930ab17707
SeaonLeague <facebookups23@gmail.com>
2022-12-07 09:38:24 -0800

    Maybe, What?

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,4 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    maxval = 0
-    maxkey = None
-    for k, v in a_dictionary.items():
-        if v > maxval:
-            maxval = v
-            maxval = k
-    return(maxkey)
+    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
