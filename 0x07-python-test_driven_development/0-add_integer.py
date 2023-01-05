9a122c46aa989c5d113d56627ca0210929dca2b0
SeaonLeague <facebookups23@gmail.com>
2023-01-05 01:20:35 -0800

    Sweet 404

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Defines an integer addition function."""
+'''Contains an add_integer function for a TDD project.
+'''
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
-    Float arguments are typecasted to ints before addition is performed.
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
-    """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
-
-if __name__ == "__main__":
-        import doctest
-        doctest.testfile("tests/0-add_integer.txt")
+    '''Computes the sum of two integers.
+    Args:
+        a (int): The first number.
+        b (int, optional): The second number.
+    Returns:
+        int: The sum of the two integers.
+    '''
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    elif not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    else:
+        return int(a) + int(b)
