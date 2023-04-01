ce1b9d4b7cf355a89ceefb9323726bf3d4d00cf9
SeaonLeague <facebookups23@gmail.com>
2023-04-01 12:54:24 -0700

    File 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-'''
-    Script that fetches https://intranet.hbtn.io/status.
-'''
+""" Script that fetches from a url using urllib package"""
 
-import urllib.request as req
+import urllib.request as request
 
 if __name__ == "__main__":
-    with req.urlopen("https://intranet.hbtn.io/status") as url:
-        if url.readable():
-            response = url.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(response)))
-            print("\t- content: {}".format(response))
-            print("\t- utf8 content: {}".format(response.decode("utf-8")))
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
