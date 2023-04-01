a82f2f374b72b39c867f46a2c35ad047a0aa2473
SeaonLeague <facebookups23@gmail.com>
2023-04-01 13:01:10 -0700

    Third

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-'''
-    Fetch url and display ststus.
-'''
+""" Uses requests module to fetch URL status"""
 
-import requests as req
+import requests
 
-if __name__ == "__main__":
-    dat = req.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(dat.text)))
-    print("\t- content: {}".format(dat.text))
+if __name__ == '__main__':
+    response = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(response.text), response.text))
