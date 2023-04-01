a67340d70c4086d1624a6cfda219c9c69cb4de25
SeaonLeague <facebookups23@gmail.com>
2023-04-01 13:03:56 -0700

    fourth

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+""" Github commits code challenge"""
 
-'''
-    List 10 commits from given repo.
-'''
+import requests
+from sys import argv
 
-import requests as req
-import sys
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}commits"\
+          .format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
 
-if __name__ == "__main__":
-    dat = req.get("https://api.github.com/repos/{0}/{1}/commits".format(sys.argv[1], sys.argv[2]))
-
-    commits = dat.json()
-    for c in commits[:10]:
-        print(c.get('sha'), end=': ')
-        print(c.get('commit').get('author').get('name'))
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
